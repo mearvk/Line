4ae41f875dea86b98683ba8e160cbd042908af1f
Max Rupplin <mearvk@outlook.com>
2017-02-19 23:05:52 -0500

    Add files via upload

---
+++ Lines.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lines;
+
+/**
+ *
+ * @author Max Rupplin
+ */
+public class Lines 
+{
+    protected final Integer hash = 0x888fe8;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        //drag and drop lines to complete code (XML pattern(s) against public interfaces to derive code quickly)
+    }
+    
+}
